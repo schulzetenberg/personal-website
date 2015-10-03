8e8bddf7365296cd4940cf16e8f196de351e1237
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2015-10-03 13:33:46 -0600

    Organize pages

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,32 @@ router.get('/index', function (req, res, next) {
 	  res.render('pages/index.html');
 });
 
+router.get('/about', function (req, res, next) {
+	  res.render('pages/about.html');
+});
+
+router.get('/album-art', function (req, res, next) {
+	  res.render('pages/album-art.html');
+});
+
+router.get('/coming-soon', function (req, res, next) {
+	  res.render('pages/coming-soon.html');
+});
+
+router.get('/projects', function (req, res, next) {
+	  res.render('pages/projects.html');
+});
+
+router.get('/senior-design', function (req, res, next) {
+	  res.render('pages/senior-design.html');
+});
+
+router.get('/resume', function (req, res, next) {
+	  res.render('pages/resume.html');
+});
+
+router.get('/website', function (req, res, next) {
+	  res.render('pages/website.html');
+});
+
 module.exports = router;
