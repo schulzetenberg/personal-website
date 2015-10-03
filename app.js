227655c64899555d6b7c0b7c6fe3c51b79731512
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2015-10-03 12:59:40 -0600

    Added logging & server.js OpenShift file

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express')
   , route = require('./routes/index')
   , user = require('./routes/user')
+  , logger = require('morgan')
   , path = require('path');
 
 var app = express();
-
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');  
 app.engine('html', require('ejs').renderFile);  //render html files as ejs
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-app.listen(app.get('port'));
-console.log('Express server listening on port ' + app.get('port'));
+//app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+//app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+//app.listen(app.get('port'));
+//console.log('Express server listening on port ' + app.get('port'));
 
 app.use('/', route);
 
@@ -23,9 +24,17 @@ app.use(function(req, res) {
    res.render('pages/404.html');
 });
 
+// Development error handler, will print stacktrace
+if (app.get('env') === 'development') {
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send('error'+ err.message );
+});
+}
 // Handle 500 error
 app.use(function(error, req, res, next) {
     res.status(500);
    res.render('pages/500.html');
 });
 
+module.exports = app;
