20546f54862b52ecbd196bb8d681907580418c22
Jacob Schulzetenberg <Jacob.Schulzetenberg@gmail.com>
2016-02-04 17:53:27 -0700

    Complete js beautify functionality

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var beautify_js  = require('js-beautify').js_beautify;
+var beautify_css = require('js-beautify').css;
+var beautify_html = require('js-beautify').html;
+
 var lastFM = require('../models/lastFM-schema');
 var goodreads = require('../models/goodreads-schema');
 
@@ -18,11 +22,27 @@ router.get('/goodreads', function (req, res, next) {
 });
 
 router.post('/beautify', function (req, res, next) {
-    console.log(req.body);
-    /*
-        TO implement: parse the body and return the beautified data
-    */
-    res.json("data");
+    var text = req.body.text;
+    var type = req.body.type;
+    var options = '';
+    var output = '';
+    if(type === 'js'){
+        try {
+             options = JSON.parse(req.body.options);
+        } catch (e){
+            options = '';
+        }
+        try{
+            output = beautify_js(text,options);
+        } catch (e){
+            return res.sendStatus(500);
+        }
+    } else if(type === 'css'){
+        // TODO: implement this
+    } else if(type === 'html'){
+        // TODO: implement this
+    }
+    res.json(output);
 });
 
 module.exports = router;
