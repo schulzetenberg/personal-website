56dafc575d6aa8a59feb4100573df5761b7ee36c
Schulzetenberg, Jacob <jacob.schulzetenberg@gmail.com>
2016-07-14 13:20:02 -0600

    update

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+var fs = require('fs');
+
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    // Check js for errors
+    'jshint': {
+      lib: [
+        'lib/*.js'
+      ]
+    },
+
+    // Concatenate all files
+    'concat': {
+      options: {},
+      styles: {
+        src: [
+          'node_modules/bootstrap/dist/css/bootstrap.min.css',
+          'node_modules/font-awesome/css/font-awesome.min.css',
+          'node_modules/flexslider/flexslider.css',
+          'lib/css/theme-aquatica.css',
+          'lib/css/custom.css'
+        ],
+        dest: 'public/css/styles.css',
+        nonull: true
+      },
+      header: {
+        src: [
+          'node_modules/jquery/dist/jquery.min.js',
+          'node_modules/angular/angular.min.js',
+          'lib/js/google-analytics.js',
+          'lib/js/loader.js',
+          'node_modules/q/q.js',
+          'node_modules/lodash/lodash.js',
+          'node_modules/d3/build/d3.min.js',
+          'node_modules/dimple/dist/dimple.latest.js'
+        ],
+        dest: 'public/js/header-plugins.js',
+        nonull: true
+      },
+      footer: {
+        src: [
+          'node_modules/bootstrap/dist/js/bootstrap.min.js',
+          'node_modules/flexslider/jquery.flexslider-min.js'
+        ],
+        dest: 'public/js/footer-plugins.js',
+        nonull: true
+      }
+    },
+
+    'uglify': {
+      header: {
+        src: 'public/js/header-plugins.js',
+        dest: 'public/js/header-plugins.js'
+      },
+      footer: {
+        src: 'public/js/footer-plugins.js',
+        dest: 'public/js/footer-plugins.js'
+      }
+    },
+
+    'cssmin': {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      styles: {
+        files: {
+          'public/css/styles.css': 'public/css/styles.css'
+        }
+      }
+    },
+
+    // Copy dependencies to public folder
+    'copy': {
+    fa: {
+      nonull: true,
+      expand: true,
+      cwd: 'node_modules/font-awesome/fonts/',
+      src: ['*.eot', '*.svg', '*.ttf', '*.woff', '*.woff2', '*.otf'],
+      dest: 'public/fonts/'
+    },
+    bs: {
+      nonull: true,
+      expand: true,
+      cwd: 'node_modules/bootstrap/fonts/',
+      src: '**',
+      dest: 'public/fonts/'
+    }
+  }
+
+  });
+
+  grunt.registerTask('default', ['jshint:lib', 'concat:styles', 'concat:header', 'concat:footer', 'cssmin:styles', 'copy:fa', 'copy:bs']);
+
+};
