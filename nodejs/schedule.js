b618c663027d95fb767d3ca45b141fb6bb07616c
Schulzetenberg, Jacob <jacob.schulzetenberg@gmail.com>
2016-08-24 22:18:25 -0600

    updates

--- nodejs/schedule.js
+++ nodejs/schedule.js
@@ -4,9 +4,14 @@ var goodreads = require('./goodreads');
 var github = require('./github');
 var lastFM = require('./last-fm');
 
-goodreads.save();
-lastFM.save();
-
 nodeSchedule.scheduleJob('10 0 0 * * *', lastFM.save); // Run daily
 nodeSchedule.scheduleJob('5 0 0 * * *', goodreads.save); // Run daily
 nodeSchedule.scheduleJob('0 0 0 * * *', github.save); // Run daily
+
+/*  // TESTING! \\
+
+lastFM.save();
+goodreads.save();
+github.save();
+
+*/
