22fc3a09311a1d023ca77cda4f0f20c97945b014
Schulzetenberg, Jacob <jacob.schulzetenberg@gmail.com>
2016-10-09 15:38:01 -0600

    trakt

--- nodejs/schedule.js
+++ nodejs/schedule.js
@@ -3,7 +3,9 @@ var nodeSchedule = require('node-schedule');
 var goodreads = require('./goodreads');
 var github = require('./github');
 var lastFM = require('./last-fm');
+var trakt = require('./trakt');
 
+nodeSchedule.scheduleJob('15 0 0 * * *', trakt.save); // Run daily
 nodeSchedule.scheduleJob('10 0 0 * * *', lastFM.save); // Run daily
 nodeSchedule.scheduleJob('5 0 0 * * *', goodreads.save); // Run daily
 nodeSchedule.scheduleJob('0 0 0 * * *', github.save); // Run daily
@@ -13,5 +15,8 @@ nodeSchedule.scheduleJob('0 0 0 * * *', github.save); // Run daily
 lastFM.save();
 goodreads.save();
 github.save();
+trakt.save();
 
 */
+
+trakt.save();
