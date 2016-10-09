59b3a0aed7456ef93f81cfeb40a7aaec72b2fb56
Schulzetenberg, Jacob <jacob.schulzetenberg@gmail.com>
2016-10-09 15:47:18 -0600

    update config

--- config/secrets.js
+++ config/secrets.js
@@ -1,15 +1,19 @@
 module.exports = {
 
   db: process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost:27017/personal-site',
-  
+
   lastFmKey: process.env.LASTFM_API_KEY || '',
-  
+
   goodreadsID: process.env.GOODREADS_ID || '',
-  
+
   goodreadsKey: process.env.GOODREADS_API_KEY || '',
-  
+
   githubUser: process.env.GITHUB_USER || 'schulzetenberg',
-  
-  githubToken: process.env.GITHUB_TOKEN || ''
+
+  githubToken: process.env.GITHUB_TOKEN || '',
+
+  traktID: process.env.TRAKT_ID || '',
+
+  traktSecret: process.env.TRAKT_SECRET || ''
 
 };
