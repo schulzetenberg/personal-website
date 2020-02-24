f6093dee03b8400a592620b0d154602d96095dbf
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2020-02-23 18:30:14 -0600

    Add API token to requests

--- src/app/shared/server.service.ts
+++ src/app/shared/server.service.ts
@@ -1,49 +1,56 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ServerService {
-  apiUrl = 'http://data.schulzetenberg.com/api';
+	apiUrl = 'http://data.schulzetenberg.com/api';
+
+	httpOptions = {
+		headers: new HttpHeaders({
+			'token': 'be6d527fb17f753f84abfa4152aff6ca', // TODO: Make this a config value
+		})
+	};
+
 
   constructor(private http: HttpClient) {}
 
   getGithubData() {
-    return this.http.get(`${this.apiUrl}/github`);
+    return this.http.get(`${this.apiUrl}/github`, this.httpOptions);
   }
 
   getMusicData() {
     return this.http
-      .get(`${this.apiUrl}/music`)
+      .get(`${this.apiUrl}/music`, this.httpOptions)
       .toPromise()
       .then((response) => response);
   }
 
   getBookData() {
     return this.http
-      .get(`${this.apiUrl}/goodreads`)
+      .get(`${this.apiUrl}/goodreads`, this.httpOptions)
       .toPromise()
       .then((response) => response);
   }
 
   getTvData() {
     return this.http
-      .get(`${this.apiUrl}/trakt`)
+      .get(`${this.apiUrl}/trakt`, this.httpOptions)
       .toPromise()
       .then((response) => response);
   }
 
   getDrivingData() {
-    return this.http.get(`${this.apiUrl}/fuelly-avg`);
+    return this.http.get(`${this.apiUrl}/fuelly-avg`, this.httpOptions);
   }
 
   getStatesData() {
     return this.http
-      .get(`${this.apiUrl}/states`)
+      .get(`${this.apiUrl}/states`, this.httpOptions)
       .toPromise()
       .then((response) => response);
   }
 
   getPodcastData() {
-    return this.http.get(`${this.apiUrl}/player-fm`);
+    return this.http.get(`${this.apiUrl}/player-fm`, this.httpOptions);
   }
 }
