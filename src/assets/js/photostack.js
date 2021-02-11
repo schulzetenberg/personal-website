41f33e25e346ea8add43c8d5230df8f9422d038a
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2021-02-10 19:04:59 -0600

    Re-render polaroids only on substantial size change

--- src/assets/js/photostack.js
+++ src/assets/js/photostack.js
@@ -1,47 +1,59 @@
-;( function( window ) {[]
+;(function(window) {[]
 
 	'use strict';
 
 	var transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed('transition') ];
+		'WebkitTransition': 'webkitTransitionEnd',
+		'MozTransition': 'transitionend',
+		'OTransition': 'oTransitionEnd',
+		'msTransition': 'MSTransitionEnd',
+		'transition': 'transitionend'
+	};
 
-	function extend( a, b ) {
-		for( var key in b ) {
-			if(b.hasOwnProperty( key )) {
+	var transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
+
+	function extend(a, b) {
+		for (let key in b) {
+			if (b.hasOwnProperty(key)) {
 				a[key] = b[key];
 			}
 		}
+
 		return a;
 	}
 
 	function shuffleMArray(marray) {
 		var arr = [], marrlen = marray.length, inArrLen = marray[0].length;
-		for(var i=0; i < marrlen; i++) {
-			arr = arr.concat( marray[i] );
+
+		for (let i=0; i < marrlen; i++) {
+			arr = arr.concat(marray[i]);
 		}
+
 		// shuffle 2 d array
-		arr = shuffleArr( arr );
+		arr = shuffleArr(arr);
 		// to 2d
-		var newmarr = [], pos = 0;
-		for( var j = 0; j < marrlen; j++ ) {
-			var tmparr = [];
-			for( var k = 0; k < inArrLen; k++ ) {
+		const newmarr = []
+		let pos = 0;
+
+		for (let j = 0; j < marrlen; j++) {
+			const tmparr = [];
+
+			for (let k = 0; k < inArrLen; k++) {
 				tmparr.push( arr[ pos ] );
 				pos++;
 			}
-			newmarr.push( tmparr );
+
+			newmarr.push(tmparr);
 		}
+
 		return newmarr;
 	}
 
 	function shuffleArr(array) {
-		var m = array.length, t, i;
+		let m = array.length;
+		let t;
+		let  i;
+
 		// While there remain elements to shuffle…
 		while (m) {
 			// Pick a remaining element…
@@ -51,21 +63,22 @@
 			array[m] = array[i];
 			array[i] = t;
 		}
+
 		return array;
 	}
 
 	function Photostack(el, options) {
 		this.el = el;
 		this.inner = this.el.querySelector('div');
-		this.allItems = [].slice.call( this.inner.children );
+		this.allItems = [].slice.call(this.inner.children);
 		this.allItemsCount = this.allItems.length;
-		if(!this.allItemsCount) return;
-		this.items = [].slice.call( this.inner.querySelectorAll('figure:not([data-dummy])') );
+		if (!this.allItemsCount) return;
+		this.items = [].slice.call(this.inner.querySelectorAll('figure:not([data-dummy])'));
 		this.itemsCount = this.items.length;
 		// index of the current photo
 		this.current = 0;
-		this.options = extend( {}, this.options );
-  		extend( this.options, options );
+		this.options = extend({}, this.options);
+  		extend(this.options, options);
   		this._init();
 	}
 
@@ -73,155 +86,100 @@
 
 	Photostack.prototype._init = function() {
 		this.currentItem = this.items[this.current];
-		this._addNavigation();
 		this._getSizes();
 		this._initEvents();
-	}
-
-	Photostack.prototype._addNavigation = function() {
-		// add nav dots
-		this.nav = document.createElement('nav')
-		var inner = '';
-		for(var i=0; i < this.itemsCount; ++i ) {
-			inner += '<span></span>';
-		}
-		this.nav.innerHTML = inner;
-		this.el.appendChild( this.nav );
-		this.navDots = [].slice.call( this.nav.children );
-	}
+	};
 
 	Photostack.prototype._initEvents = function() {
-		var self = this,
-			beforeStep = this.el.classList.contains('photostack-start'),
-			open = function() {
-				var setTransition = function() {
-					self.el.classList.add('photostack-transition');
-				}
-				if(beforeStep) {
-					this.removeEventListener('click', open);
-					self.el.classList.remove('photostack-start');
-					setTransition();
-				} else {
-					self.openDefault = true;
-					setTimeout( setTransition, 25 );
-				}
-				self.started = true;
-				self._showPhoto( self.current );
-			};
+		var self = this;
 
-		if(beforeStep) {
-			this._shuffle();
-			this.el.addEventListener('click', open);
-		} else {
-			open();
-		}
+		this.started = true;
+		this._showPhoto(self.current);
 
-		this.navDots.forEach(function(dot, idx) {
-			dot.addEventListener('click', function() {
-				// rotate the photo if clicking on the current dot
-				if(idx === self.current) {
-					self._rotateItem();
-				} else {
-					// if the photo is flipped then rotate it back before shuffling again
-					var callback = function() { self._showPhoto( idx ); }
-					if(self.flipped) {
-						self._rotateItem( callback );
-					} else {
-						callback();
-					}
-				}
-			} );
-		} );
 
-		window.addEventListener('resize', function() { self._resizeHandler(); });
-	}
+		window.addEventListener('resize', function() {
+			self._resizeHandler();
+		});
+	};
 
 	Photostack.prototype._resizeHandler = function() {
 		var self = this;
+
 		function delayed() {
 			self._resize();
 			self._resizeTimeout = null;
 		}
-		if (this._resizeTimeout) clearTimeout( this._resizeTimeout );
-		this._resizeTimeout = setTimeout( delayed, 100 );
+
+		if (this._resizeTimeout) clearTimeout(this._resizeTimeout);
+		this._resizeTimeout = setTimeout(delayed, 200);
 	}
 
 	Photostack.prototype._resize = function() {
-		var self = this, callback = function() { self._shuffle( true ); }
-		this._getSizes();
-		if(this.started && this.flipped) {
-			this._rotateItem( callback );
-		}	else {
-			callback();
-		}
-	}
+		// Check if div size is 75 pixels different than the previous render.
+		// Only re-render if substantial difference to fix mobile Chrome resize on navbar hide
+		const changedHeight = Math.abs(this.sizes.inner.height - this.inner.offsetHeight) > 75;
+		const changedWidth = Math.abs(this.sizes.inner.width - this.inner.offsetWidth) > 75;
 
-	Photostack.prototype._showPhoto = function( pos ) {
-		if(this.isShuffling) return false;
+		console.log(changedHeight, changedWidth);
+
+		if (changedHeight || changedWidth) {
+			this._getSizes();
+			this._shuffle(true);
+		}
+	};
+
+	Photostack.prototype._showPhoto = function(pos) {
+		if (this.isShuffling) return false;
+
 		this.isShuffling = true;
-
-		// if there is something behind..
-		if(this.currentItem.classList.contains('photostack-flip')) {
-			this._removeItemPerspective();
-			this.navDots[this.current].classList.remove('flippable')
-		}
-
-		this.navDots[this.current].classList.remove('current');
 		this.currentItem.classList.remove('photostack-current');
 
 		// change current
 		this.current = pos;
-		this.currentItem = this.items[ this.current ];
-
-		this.navDots[this.current].classList.add('current');
-		// if there is something behind..
-		if(this.currentItem.querySelector('.photostack-back') ) this.navDots[pos].classList.add('flippable'); // nav dot gets class flippable
-
-		// shuffle a bit
+		this.currentItem = this.items[this.current];
 		this._shuffle();
 	}
 
 	// display items (randomly)
 	Photostack.prototype._shuffle = function(resize) {
-		var iter = resize ? 1 : this.currentItem.getAttribute( 'data-shuffle-iteration' ) || 1;
-		if(iter <= 0 || !this.started || this.openDefault) { iter = 1; }
-		// first item is open by default
-		if(this.openDefault) {
-			// change transform-origin
-			this.currentItem.classList.add('photostack-flip');
-			this.openDefault = false;
-			this.isShuffling = false;
+		var iter = resize ? 1 : this.currentItem.getAttribute('data-shuffle-iteration') || 1;
+
+		if (iter <= 0 || !this.started) {
+			iter = 1;
 		}
 
-		var overlapFactor = .5,
+		var overlapFactor = .5;
 			// lines & columns
-			lines = Math.ceil(this.sizes.inner.width / (this.sizes.item.width * overlapFactor) ),
-			columns = Math.ceil(this.sizes.inner.height / (this.sizes.item.height * overlapFactor) ),
+		var	lines = Math.ceil(this.sizes.inner.width / (this.sizes.item.width * overlapFactor) );
+		var columns = Math.ceil(this.sizes.inner.height / (this.sizes.item.height * overlapFactor) );
 			// since we are rounding up the previous calcs we need to know how much more we are adding to the calcs for both x and y axis
-			addX = lines * this.sizes.item.width * overlapFactor + this.sizes.item.width/2 - this.sizes.inner.width,
-			addY = columns * this.sizes.item.height * overlapFactor + this.sizes.item.height/2 - this.sizes.inner.height,
+		var addX = lines * this.sizes.item.width * overlapFactor + this.sizes.item.width/2 - this.sizes.inner.width;
+		var addY = columns * this.sizes.item.height * overlapFactor + this.sizes.item.height/2 - this.sizes.inner.height;
 			// we will want to center the grid
-			extraX = addX / 2,
-			extraY = addY / 2,
+		var extraX = addX / 2;
+		var extraY = addY / 2;
 			// max and min rotation angles
-			maxrot = 35, minrot = -35,
-			self = this,
+		var maxrot = 35;
+		var minrot = -35;
+		var self = this;
 			// translate/rotate items
-			moveItems = function() {
+		var moveItems = function() {
 				--iter;
 				// create a "grid" of possible positions
 				var grid = [];
 				// populate the positions grid
-				for( var i = 0; i < columns; ++i ) {
+				for (var i = 0; i < columns; ++i) {
 					var col = grid[ i ] = [];
-					for( var j = 0; j < lines; ++j ) {
+
+					for (var j = 0; j < lines; ++j) {
 						var xVal = j * (self.sizes.item.width * overlapFactor) - extraX,
 							yVal = i * (self.sizes.item.height * overlapFactor) - extraY,
 							olx = 0, oly = 0;
 
-						if(self.started && iter === 0) {
+						if (self.started && iter === 0) {
 							var ol = self._isOverlapping( { x : xVal, y : yVal } );
-							if(ol.overlapping) {
+
+							if (ol.overlapping) {
 								olx = ol.noOverlap.x;
 								oly = ol.noOverlap.y;
 								var r = Math.floor( Math.random() * 3 );
@@ -232,61 +190,67 @@
 							}
 						}
 
-						col[ j ] = { x : xVal + olx, y : yVal + oly };
+						col[j] = {
+							x: xVal + olx,
+							y: yVal + oly
+						};
 					}
 				}
+
 				// shuffle
 				grid = shuffleMArray(grid);
 
-				var l = 0, c = 0, cntItemsAnim = 0;
-				self.allItems.forEach( function( item, i ) {
+				var l = 0
+				var c = 0;
+				var cntItemsAnim = 0;
+
+				self.allItems.forEach(function(item, i) {
 					// pick a random item from the grid
-					if(l === lines - 1) {
+					if (l === lines - 1) {
 						c = c === columns - 1 ? 0 : c + 1;
 						l = 1;
 					} else {
 						++l
 					}
 
-					var randXPos = Math.floor( Math.random() * lines ),
-						randYPos = Math.floor( Math.random() * columns ),
-						gridVal = grid[c][l-1],
-						translation = { x : gridVal.x, y : gridVal.y },
-						onEndTransitionFn = function() {
-							++cntItemsAnim;
-							this.removeEventListener( transEndEventName, onEndTransitionFn );
-							if(cntItemsAnim === self.allItemsCount) {
-								if(iter > 0) {
-									moveItems.call();
-								} else {
-									// change transform-origin
-									self.currentItem.classList.add('photostack-flip');
-									// all done..
-									self.isShuffling = false;
-									if(typeof self.options.callback === 'function') {
-										self.options.callback( self.currentItem );
-									}
+					var randXPos = Math.floor(Math.random() * lines);
+					var randYPos = Math.floor(Math.random() * columns);
+					var gridVal = grid[c][l-1];
+					var translation = {
+						x: gridVal.x,
+						y: gridVal.y
+					};
+
+					var onEndTransitionFn = function() {
+						++cntItemsAnim;
+						this.removeEventListener(transEndEventName, onEndTransitionFn);
+
+						if (cntItemsAnim === self.allItemsCount) {
+							if (iter > 0) {
+								moveItems.call();
+							} else {
+								self.isShuffling = false;
+
+								if (typeof self.options.callback === 'function') {
+									self.options.callback(self.currentItem);
 								}
 							}
-						};
+						}
+					};
 
-					if(self.items.indexOf(item) === self.current && self.started && iter === 0) {
+					if (self.items.indexOf(item) === self.current && self.started && iter === 0) {
 						self.currentItem.style.WebkitTransform = 'translate(' + self.centerItem.x + 'px,' + self.centerItem.y + 'px) rotate(0deg)';
 						self.currentItem.style.msTransform = 'translate(' + self.centerItem.x + 'px,' + self.centerItem.y + 'px) rotate(0deg)';
 						self.currentItem.style.transform = 'translate(' + self.centerItem.x + 'px,' + self.centerItem.y + 'px) rotate(0deg)';
-						// if there is something behind..
-						if(self.currentItem.querySelector('.photostack-back')) {
-							self._addItemPerspective();
-						}
 						self.currentItem.classList.add('photostack-current');
 					} else {
-						item.style.WebkitTransform = 'translate(' + translation.x + 'px,' + translation.y + 'px) rotate(' + Math.floor( Math.random() * (maxrot - minrot + 1) + minrot ) + 'deg)';
-						item.style.msTransform = 'translate(' + translation.x + 'px,' + translation.y + 'px) rotate(' + Math.floor( Math.random() * (maxrot - minrot + 1) + minrot ) + 'deg)';
-						item.style.transform = 'translate(' + translation.x + 'px,' + translation.y + 'px) rotate(' + Math.floor( Math.random() * (maxrot - minrot + 1) + minrot ) + 'deg)';
+						item.style.WebkitTransform = 'translate(' + translation.x + 'px,' + translation.y + 'px) rotate(' + Math.floor(Math.random() * (maxrot - minrot + 1) + minrot) + 'deg)';
+						item.style.msTransform = 'translate(' + translation.x + 'px,' + translation.y + 'px) rotate(' + Math.floor(Math.random() * (maxrot - minrot + 1) + minrot) + 'deg)';
+						item.style.transform = 'translate(' + translation.x + 'px,' + translation.y + 'px) rotate(' + Math.floor(Math.random() * (maxrot - minrot + 1) + minrot) + 'deg)';
 					}
 
 					if(self.started) {
-						item.addEventListener( transEndEventName, onEndTransitionFn );
+						item.addEventListener(transEndEventName, onEndTransitionFn);
 					}
 				} );
 			};
@@ -296,79 +260,53 @@
 
 	Photostack.prototype._getSizes = function() {
 		this.sizes = {
-			inner : { width : this.inner.offsetWidth, height : this.inner.offsetHeight },
-			item : { width : this.currentItem.offsetWidth, height : this.currentItem.offsetHeight }
+			inner: {
+				width: this.inner.offsetWidth,
+				height: this.inner.offsetHeight
+			},
+			item: {
+				width: this.currentItem.offsetWidth,
+				height: this.currentItem.offsetHeight
+			}
 		};
 
 		// translation values to center an item
-		this.centerItem = { x : this.sizes.inner.width / 2 - this.sizes.item.width / 2, y : this.sizes.inner.height / 2 - this.sizes.item.height / 2 };
+		this.centerItem = {
+			x: this.sizes.inner.width / 2 - this.sizes.item.width / 2,
+			y: this.sizes.inner.height / 2 - this.sizes.item.height / 2
+		};
 	}
 
 	Photostack.prototype._isOverlapping = function(itemVal) {
-		var dxArea = this.sizes.item.width + this.sizes.item.width / 3, // adding some extra avoids any rotated item to touch the central area
-			dyArea = this.sizes.item.height + this.sizes.item.height / 3,
-			areaVal = { x : this.sizes.inner.width / 2 - dxArea / 2, y : this.sizes.inner.height / 2 - dyArea / 2 },
-			dxItem = this.sizes.item.width,
-			dyItem = this.sizes.item.height;
+		var dxArea = this.sizes.item.width + this.sizes.item.width / 3; // adding some extra avoids any rotated item to touch the central area
+		var dyArea = this.sizes.item.height + this.sizes.item.height / 3;
+		var areaVal = {
+			x: this.sizes.inner.width / 2 - dxArea / 2,
+			y: this.sizes.inner.height / 2 - dyArea / 2
+		};
+		var dxItem = this.sizes.item.width;
+		var dyItem = this.sizes.item.height;
 
-		if( !(( itemVal.x + dxItem ) < areaVal.x ||
+		if (!(( itemVal.x + dxItem ) < areaVal.x ||
 			itemVal.x > ( areaVal.x + dxArea ) ||
 			( itemVal.y + dyItem ) < areaVal.y ||
 			itemVal.y > ( areaVal.y + dyArea )) ) {
 				// how much to move so it does not overlap?
 				// move left / or move right
-				var left = Math.random() < 0.5,
-					randExtraX = Math.floor( Math.random() * (dxItem/4 + 1) ),
-					randExtraY = Math.floor( Math.random() * (dyItem/4 + 1) ),
-					noOverlapX = left ? (itemVal.x - areaVal.x + dxItem) * -1 - randExtraX : (areaVal.x + dxArea) - (itemVal.x + dxItem) + dxItem + randExtraX,
-					noOverlapY = left ? (itemVal.y - areaVal.y + dyItem) * -1 - randExtraY : (areaVal.y + dyArea) - (itemVal.y + dyItem) + dyItem + randExtraY;
+				var left = Math.random() < 0.5;
+				var randExtraX = Math.floor( Math.random() * (dxItem/4 + 1) );
+				var randExtraY = Math.floor( Math.random() * (dyItem/4 + 1) );
+				var noOverlapX = left ? (itemVal.x - areaVal.x + dxItem) * -1 - randExtraX : (areaVal.x + dxArea) - (itemVal.x + dxItem) + dxItem + randExtraX;
+				var noOverlapY = left ? (itemVal.y - areaVal.y + dyItem) * -1 - randExtraY : (areaVal.y + dyArea) - (itemVal.y + dyItem) + dyItem + randExtraY;
 
 				return {
 					overlapping : true,
-					noOverlap : { x : noOverlapX, y : noOverlapY }
-				}
-		}
-		return {
-			overlapping : false
-		}
-	}
-
-	Photostack.prototype._addItemPerspective = function() {
-		this.el.classList.add('photostack-perspective');
-	}
-
-	Photostack.prototype._removeItemPerspective = function() {
-		this.el.classList.remove('photostack-perspective');
-		this.currentItem.classList.remove('photostack-flip');
-	}
-
-	Photostack.prototype._rotateItem = function( callback ) {
-		if(this.el.classList.contains('photostack-perspective') && !this.isRotating && !this.isShuffling) {
-
-			this.isRotating = true;
-
-			var self = this, onEndTransitionFn = function() {
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-					self.isRotating = false;
-					if(typeof callback === 'function') {
-						callback();
-					}
+					noOverlap: { x: noOverlapX, y: noOverlapY }
 				};
-
-			if(this.flipped) {
-				this.navDots[this.current].classList.remove('flip');
-				this.currentItem.style.WebkitTransform = 'translate(' + this.centerItem.x + 'px,' + this.centerItem.y + 'px) rotateY(0deg)';
-				this.currentItem.style.transform = 'translate(' + this.centerItem.x + 'px,' + this.centerItem.y + 'px) rotateY(0deg)';
-			}	else {
-				this.navDots[this.current].classList.add('flip');
-				this.currentItem.style.WebkitTransform = 'translate(' + this.centerItem.x + 'px,' + this.centerItem.y + 'px) translate(' + this.sizes.item.width + 'px) rotateY(-179.9deg)';
-				this.currentItem.style.transform = 'translate(' + this.centerItem.x + 'px,' + this.centerItem.y + 'px) translate(' + this.sizes.item.width + 'px) rotateY(-179.9deg)';
-			}
-
-			this.flipped = !this.flipped;
-			this.currentItem.addEventListener( transEndEventName, onEndTransitionFn );
 		}
-	}
+
+		return { overlapping: false };
+	};
 
 	// add to global namespace
 	window.Photostack = Photostack;
