0048f8f652b3eabac9b60e46f66d469a7c8023ce
Jacob <jacob.schulzetenberg@gmail.com>
2022-10-09 21:52:44 -0500

    Book animation

--- src/app/about/books/recent-books/recent-books.component.ts
+++ src/app/about/books/recent-books/recent-books.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { gsap } from 'gsap';
+// import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 import { ServerService } from '../../../shared/server.service';
 
 @Component({
@@ -9,11 +11,21 @@ import { ServerService } from '../../../shared/server.service';
 export class RecentBooksComponent implements OnInit {
   bookData: any;
 
+  @ViewChildren('book') bookContainers: QueryList<ElementRef>;
+
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
+    // gsap.registerPlugin(CSSRulePlugin);
+
     this.serverService.getBookData().then((data) => {
       this.bookData = data;
+
+      setTimeout(() => {
+        const bookArr: Element[] = this.bookContainers.map((x) => x.nativeElement);
+        const tl = gsap.timeline({ defaults: { ease: 'power4.inOut', duration: 0.6 } });
+        tl.to(bookArr, { stagger: 0.1, opacity: 1 });
+      }, 5);
     });
   }
 }
--- src/app/full-page-header/full-page-header.component.ts
+++ src/app/full-page-header/full-page-header.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { gsap } from 'gsap';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-full-page-header',
   templateUrl: './full-page-header.component.html',
   styleUrls: ['./full-page-header.component.scss'],
 })
-export class FullPageHeaderComponent implements OnInit {
+export class FullPageHeaderComponent implements AfterViewInit {
   // eslint-disable-next-line
   constructor() {}
 
-  // eslint-disable-next-line
-  ngOnInit() {
+  @ViewChild('headshot') chidlHeadshot: ElementRef;
+
+  @ViewChild('h1') childH1: ElementRef;
+
+  @ViewChild('h2') childH2: ElementRef;
+
+  ngAfterViewInit() {
     const tl = gsap.timeline({ defaults: { ease: 'power4.inOut', duration: 1, opacity: 1, y: 0 } });
-    tl.to('#headshot', {});
-    tl.to('h1', { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', scale: 1.25 }, '-=1');
-    tl.to('h2', { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=1');
+    const headshot: Element = this.chidlHeadshot.nativeElement;
+    const h1: Element = this.childH1.nativeElement;
+    const h2: Element = this.childH2.nativeElement;
+
+    tl.to(headshot, {});
+    tl.to(h1, { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', scale: 1.25 }, '-=1');
+    tl.to(h2, { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' }, '-=1');
   }
 }
