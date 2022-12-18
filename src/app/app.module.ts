ce09116bcb042db8de26534a1abe626e396b7710
Jacob <jacob.schulzetenberg@gmail.com>
2022-12-18 15:22:53 -0600

    Minimalist home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 import { AboutComponent } from './about/about.component';
 import { AllocationsComponent } from './about/allocations/allocations.component';
+import { DiversificationComponent } from './about/allocations/diversification/diversification.component';
 import { BooksComponent } from './about/books/books.component';
 import { RecentBooksComponent } from './about/books/recent-books/recent-books.component';
 import { DrivingComponent } from './about/driving/driving.component';
@@ -23,13 +24,13 @@ import { AppComponent } from './app.component';
 import { Error500Component } from './error-500/error-500.component';
 import { FooterComponent } from './footer/footer.component';
 import { FullPageHeaderComponent } from './full-page-header/full-page-header.component';
+import { HomeComponent } from './home/home.component';
 import { NotFound404Component } from './not-found-404/not-found-404.component';
 import { ServerService } from './shared/server.service';
 import { UsesComponent } from './uses/uses.component';
-import { DiversificationComponent } from './about/allocations/diversification/diversification.component';
 
 const appRoutes: Routes = [
-  { path: '', component: AboutComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: '500', component: Error500Component },
   { path: '404', component: NotFound404Component },
@@ -60,6 +61,7 @@ const appRoutes: Routes = [
     ParksComponent,
     AllocationsComponent,
     DiversificationComponent,
+    HomeComponent,
   ],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule, Ng2GoogleChartsModule],
   schemas: [NO_ERRORS_SCHEMA],
