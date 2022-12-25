0876f3208aa777be8dbaadf3c809391c55213849
Jacob <jacob.schulzetenberg@gmail.com>
2022-12-24 20:39:55 -0600

    Clean up unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,68 +2,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { Ng2GoogleChartsModule } from 'ng2-google-charts';
-import { AboutComponent } from './about/about.component';
-import { AllocationsComponent } from './about/allocations/allocations.component';
-import { DiversificationComponent } from './about/allocations/diversification/diversification.component';
-import { BooksComponent } from './about/books/books.component';
-import { RecentBooksComponent } from './about/books/recent-books/recent-books.component';
-import { DrivingComponent } from './about/driving/driving.component';
-import { GithubComponent } from './about/github/github.component';
 import { InstagramComponent } from './about/instagram/instagram.component';
-import { CountriesComponent } from './about/maps/countries/countries.component';
-import { StatesComponent } from './about/maps/states/states.component';
-import { MusicComponent } from './about/music/music.component';
-import { TopArtistsComponent } from './about/music/top-artists/top-artists.component';
-import { ParksComponent } from './about/parks/parks.component';
-import { PodcastsComponent } from './about/podcasts/podcasts.component';
-import { ProjectsComponent } from './about/projects/projects.component';
-import { ResumeComponent } from './about/resume/resume.component';
-import { TvComponent } from './about/tv/tv.component';
 import { AppComponent } from './app.component';
-import { Error500Component } from './error-500/error-500.component';
 import { FooterComponent } from './footer/footer.component';
 import { FullPageHeaderComponent } from './full-page-header/full-page-header.component';
 import { HomeComponent } from './home/home.component';
-import { NotFound404Component } from './not-found-404/not-found-404.component';
 import { ServerService } from './shared/server.service';
-import { UsesComponent } from './uses/uses.component';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '500', component: Error500Component },
-  { path: '404', component: NotFound404Component },
-];
+const appRoutes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    FullPageHeaderComponent,
-    NotFound404Component,
-    Error500Component,
-    AboutComponent,
-    MusicComponent,
-    GithubComponent,
-    BooksComponent,
-    PodcastsComponent,
-    TvComponent,
-    StatesComponent,
-    CountriesComponent,
-    DrivingComponent,
-    InstagramComponent,
-    ResumeComponent,
-    ProjectsComponent,
-    UsesComponent,
-    RecentBooksComponent,
-    TopArtistsComponent,
-    ParksComponent,
-    AllocationsComponent,
-    DiversificationComponent,
-    HomeComponent,
-  ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule, Ng2GoogleChartsModule],
+  declarations: [AppComponent, FooterComponent, FullPageHeaderComponent, InstagramComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [ServerService],
   bootstrap: [AppComponent],
--- src/app/shared/server.service.ts
+++ src/app/shared/server.service.ts
@@ -14,47 +14,7 @@ export class ServerService {
   // eslint-disable-next-line
   constructor(private http: HttpClient) {}
 
-  getGithubData() {
-    return this.http.get(`${this.apiUrl}/github`, this.httpOptions);
-  }
-
-  getMusicData() {
-    return this.http.get(`${this.apiUrl}/music`, this.httpOptions).toPromise();
-  }
-
-  getBookData() {
-    return this.http.get(`${this.apiUrl}/goodreads`, this.httpOptions).toPromise();
-  }
-
-  getTvData() {
-    return this.http.get(`${this.apiUrl}/trakt`, this.httpOptions).toPromise();
-  }
-
-  getDrivingData() {
-    return this.http.get(`${this.apiUrl}/fuelly-avg`, this.httpOptions);
-  }
-
-  getStatesData() {
-    return this.http.get(`${this.apiUrl}/states`, this.httpOptions).toPromise();
-  }
-
-  getCountriesData() {
-    return this.http.get(`${this.apiUrl}/countries`, this.httpOptions).toPromise();
-  }
-
-  getParksData() {
-    return this.http.get(`${this.apiUrl}/parks`, this.httpOptions).toPromise();
-  }
-
-  getPodcastData() {
-    return this.http.get(`${this.apiUrl}/player-fm`, this.httpOptions);
-  }
-
   getInstagramData() {
     return this.http.get(`${this.apiUrl}/instagram`, this.httpOptions);
-  }
-
-  getAllocationData() {
-    return this.http.get(`${this.apiUrl}/allocation`, this.httpOptions).toPromise();
   }
 }
