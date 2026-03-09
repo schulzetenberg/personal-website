4e7664d2cd7ee3355a0ee5016baec6c002b737ac
Jacob <jacob.schulzetenberg@gmail.com>
2026-03-08 23:14:09 -0500

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`scroll-smooth ${display.variable} ${mono.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
